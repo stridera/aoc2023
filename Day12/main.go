68a44d9048bc11400755916219cc7d26b35fd3ce
Matthew Jones <matthew.jones@ionq.co>
2023-12-14 12:11:36 -0500

    day12

--- Day12/main.go
+++ Day12/main.go
@@ -7,6 +7,93 @@ import (
 	"strings"
 )
 
+type Maint struct {
+	springs []rune
+	groups  []int
+}
+
+func parseData(data string, part2 bool) []Maint {
+	maints := []Maint{}
+	for _, line := range strings.Split(data, "\n") {
+		if line == "" {
+			continue
+		}
+		parts := strings.Split(line, " ")
+		springs := []rune(parts[0])
+		groups := []int{}
+		for _, s := range strings.Split(parts[1], ",") {
+			if s == "" {
+				continue
+			}
+			i, _ := strconv.Atoi(s)
+			groups = append(groups, i)
+		}
+
+		maint := Maint{}
+		maint.springs = springs
+		maint.groups = groups
+		if part2 {
+			for i := 0; i < 4; i++ {
+				maint.springs = append(maint.springs, '?')
+				maint.springs = append(maint.springs, springs...)
+				maint.groups = append(maint.groups, groups...)
+			}
+		}
+
+		maints = append(maints, maint)
+	}
+	return maints
+}
+
+var cache = make(map[string]int)
+
+func check(maint Maint, loc int, bloc int, step int) int {
+	key := fmt.Sprintf("%d-%d-%d", loc, bloc, step)
+	if v, ok := cache[key]; ok {
+		return v
+	}
+
+	// fmt.Println("Check: ", string(maint.springs), ":", maint.groups, "Loc: ", loc, "BLoc: ", bloc, "Step: ", step)
+	if loc == len(maint.springs) {
+		if bloc == len(maint.groups) && step == 0 {
+			return 1
+		} else if bloc == len(maint.groups)-1 && maint.groups[bloc] == step {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	sum := 0
+	blocks := []rune{'.', '#'}
+	for _, block := range blocks {
+		// fmt.Println("Check: ", string(maint.springs), "Loc: ", loc, "Block: ", string(block), "BLoc: ", bloc, "Step: ", step)
+		if maint.springs[loc] == block || maint.springs[loc] == '?' {
+			if block == '.' && step == 0 {
+				sum += check(maint, loc+1, bloc, 0)
+			} else if block == '.' && step > 0 && bloc < len(maint.groups) && maint.groups[bloc] == step {
+				sum += check(maint, loc+1, bloc+1, 0)
+			} else if block == '#' {
+				sum += check(maint, loc+1, bloc, step+1)
+			}
+		}
+	}
+	cache[key] = sum
+	return sum
+}
+
+func calc(maints []Maint) int {
+	sum := 0
+	for _, maint := range maints {
+		cache = make(map[string]int)
+		score := check(maint, 0, 0, 0)
+		// fmt.Println("Spring: ", string(maint.springs), "Groups: ", maint.groups, "Score: ", score)
+		sum += score
+	}
+
+	return sum
+}
+
 const testData = `???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
@@ -14,151 +101,18 @@ const testData = `???.### 1,1,3
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1`
 
-type Maint struct {
-	springs []rune
-	groups  []int
-}
-
-func parseData(data string) []Maint {
-	maints := []Maint{}
-	for _, line := range strings.Split(data, "\n") {
-		if line == "" {
-			continue
-		}
-		parts := strings.Split(line, " ")
-		maint := Maint{}
-		maint.springs = []rune(parts[0])
-		maint.groups = []int{}
-		for _, s := range strings.Split(parts[1], ",") {
-			if s == "" {
-				continue
-			}
-			i, _ := strconv.Atoi(s)
-			maint.groups = append(maint.groups, i)
-		}
-		maints = append(maints, maint)
-	}
-	return maints
-}
-
-func createPerms(springs []rune) [][]rune {
-	fmt.Println("Create Perms: ", string(springs))
-
-	perms := [][]rune{}
-	for _, spring := range springs {
-		new_perms := [][]rune{}
-		if spring == '?' {
-			if len(perms) == 0 {
-				new_perms = append(new_perms, []rune{'.'})
-				new_perms = append(new_perms, []rune{'#'})
-			} else {
-				for _, p := range perms {
-					new_perms = append(new_perms, [][]rune{append(p, 'd'), append(p, 'h')}...)
-				}
-			}
-		} else {
-			if len(perms) == 0 {
-				new_perms = append(new_perms, []rune{spring})
-			} else {
-				for _, p := range perms {
-					new_perms = append(new_perms, append(p, 'E'))
-				}
-			}
-		}
-		fmt.Println("Spring: ", string(spring), "Perms: ", len(new_perms))
-		for _, p := range new_perms {
-			fmt.Println(string(p))
-		}
-		perms = [][]rune{}
-		for _, p := range new_perms {
-			perms = append(perms, p)
-		}
-	}
-	// Remove Dups
-	seen := map[string]bool{}
-	for _, perm := range perms {
-		seen[string(perm)] = true
-	}
-	perms = [][]rune{}
-	for k := range seen {
-		perms = append(perms, []rune(k))
-	}
-	// fmt.Println("Perms: ", len(perms))
-	for _, p := range perms {
-		fmt.Println(string(p))
-	}
-	return perms
-
-}
-
-func isValid(springs []rune, groups []int) bool {
-	for {
-		if len(springs) == 0 && len(groups) == 0 {
-			return true
-		} else if len(springs) == 0 {
-			return false
-		}
-
-		if springs[0] == '.' {
-			springs = springs[1:]
-		} else if springs[0] == '#' {
-			group := groups[0]
-			if len(springs) < group {
-				return false
-			}
-			for i := 0; i < group; i++ {
-				if springs[i] != '#' {
-					return false
-				}
-			}
-			springs = springs[group:]
-			groups = groups[1:]
-			if len(springs) == 0 && len(groups) == 0 {
-				return true
-			} else if len(springs) > 1 && springs[0] == '#' {
-				return false
-			}
-
-		}
-	}
-}
-
-func part1(maints []Maint) int {
-	createPerms([]rune("#?#?#?#"))
-	return 0
-
-	sum := 0
-	for _, maint := range maints {
-		fmt.Println("Spring: ", string(maint.springs), "Groups: ", maint.groups)
-		perms := createPerms(maint.springs)
-		for _, perm := range perms {
-			// fmt.Println("- ", string(perm))
-
-			if isValid(perm, maint.groups) {
-				fmt.Println(string(perm), maint.groups, " is valid")
-				sum++
-			}
-		}
-	}
-
-	return sum
-}
-
-func part2(maints []Maint) int {
-	return 0
-}
-
 func test() {
-	maints := parseData(testData)
-	p1 := part1(maints)
+	maints := parseData(testData, false)
+	p1 := calc(maints)
 	fmt.Println("TEST Part 1: ", p1, "Expects: 21 Pass? ", p1 == 21)
-	p2 := part2(maints)
-	fmt.Println("TEST Part 2: ", p2, "Expects: 1030 Pass? ", p2 == 1030)
+	maints = parseData(testData, true)
+	p2 := calc(maints)
+	fmt.Println("TEST Part 2: ", p2, "Expects: 525152 Pass? ", p2 == 525152)
 }
 
 func main() {
 	test()
-	return
+	// return
 
 	// read file
 	data, err := os.ReadFile("input.txt")
@@ -167,10 +121,11 @@ func main() {
 		return
 	}
 
-	maints := parseData(string(data))
-	p1 := part1(maints)
+	maints := parseData(string(data), false)
+	p1 := calc(maints)
 	fmt.Println("Part 1: ", p1)
 
-	p2 := part2(maints)
+	maints = parseData(string(data), true)
+	p2 := calc(maints)
 	fmt.Println("Part 2: ", p2)
 }
